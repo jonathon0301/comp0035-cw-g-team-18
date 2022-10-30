c0672ced39eedea18bbb0a234b32539b9650836e
JonathonShi <71091633+JonathonShi@users.noreply.github.com>
2022-10-30 13:09:33 +0000

    shortened for loop

--- coursework1/data_prep.py
+++ coursework1/data_prep.py
@@ -10,9 +10,10 @@ df_5 = pd.read_csv('/Users/shishengjie/Desktop/comp0035-cw-g-team-18/coursework1
 df_6 = pd.read_csv('/Users/shishengjie/Desktop/comp0035-cw-g-team-18/coursework1/Gender_Pay_Gap/UK Gender Pay Gap Data - 2022 to 2023-3.csv')
 dfs = [df_1, df_2, df_3, df_4, df_5, df_6]
 for df in dfs:
-    print(df.shape)
-    print(df.columns)
-    print(df.dtypes)
+    print(df.shape, df.columns, df.dtypes)
+
+#Merge datasets into a single large one and save
 df_merge = pd.concat([df_1, df_2, df_3, df_4, df_5, df_6], axis = 0)
 print(df_merge.shape, df_merge.columns)
 df.to_csv(r'/Users/shishengjie/Desktop/comp0035-cw-g-team-18/coursework1/gender_pay_gap_initial.csv')
+
