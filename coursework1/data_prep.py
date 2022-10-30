701dd7d15073d40efa8c5a6292850f0aad9fac52
JonathonShi <71091633+JonathonShi@users.noreply.github.com>
2022-10-30 02:45:21 +0000

    Load datasets to data_prep

---
+++ coursework1/data_prep.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import pandas as pd
+
+#Load initial datasets
+Gender_Pay_Gap_17_18 = pd.read_csv('/Users/shishengjie/Desktop/comp0035-cw-g-team-18/coursework1/Gender_Pay_Gap/UK Gender Pay Gap Data - 2017 to 2018.csv')
+Gender_Pay_Gap_18_19 = pd.read_csv('/Users/shishengjie/Desktop/comp0035-cw-g-team-18/coursework1/Gender_Pay_Gap/UK Gender Pay Gap Data - 2018 to 2019.csv')
+Gender_Pay_Gap_19_20 = pd.read_csv('/Users/shishengjie/Desktop/comp0035-cw-g-team-18/coursework1/Gender_Pay_Gap/UK Gender Pay Gap Data - 2019 to 2020.csv')
+Gender_Pay_Gap_20_21 = pd.read_csv('/Users/shishengjie/Desktop/comp0035-cw-g-team-18/coursework1/Gender_Pay_Gap/UK Gender Pay Gap Data - 2020 to 2021.csv')
+Gender_Pay_Gap_21_22 = pd.read_csv('/Users/shishengjie/Desktop/comp0035-cw-g-team-18/coursework1/Gender_Pay_Gap/UK Gender Pay Gap Data - 2021 to 2022-2.csv')
+Gender_Pay_Gap_22_23 = pd.read_csv('/Users/shishengjie/Desktop/comp0035-cw-g-team-18/coursework1/Gender_Pay_Gap/UK Gender Pay Gap Data - 2022 to 2023-3.csv')
