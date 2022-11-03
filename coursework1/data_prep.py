e59d2b32c4a6a65eaf3dd3830a810a71a9ebda94
JonathonShi <71091633+JonathonShi@users.noreply.github.com>
2022-11-03 20:16:57 +0000

    Change path for sharing use

--- coursework1/data_prep.py
+++ coursework1/data_prep.py
@@ -6,18 +6,12 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 
 # Load initial datasets
-df_1 = pd.read_csv(
-    'Gender_Pay_Gap/UK Gender Pay Gap Data - 2017 to 2018.csv')
-df_2 = pd.read_csv(
-    'Gender_Pay_Gap/UK Gender Pay Gap Data - 2018 to 2019.csv')
-df_3 = pd.read_csv(
-    'Gender_Pay_Gap/UK Gender Pay Gap Data - 2019 to 2020.csv')
-df_4 = pd.read_csv(
-    'Gender_Pay_Gap/UK Gender Pay Gap Data - 2020 to 2021.csv')
-df_5 = pd.read_csv(
-    'Gender_Pay_Gap/UK Gender Pay Gap Data - 2021 to 2022-2.csv')
-df_6 = pd.read_csv(
-    'Gender_Pay_Gap/UK Gender Pay Gap Data - 2022 to 2023-3.csv')
+df_1 = pd.read_csv('Gender_Pay_Gap/UK Gender Pay Gap Data - 2017 to 2018.csv')
+df_2 = pd.read_csv('Gender_Pay_Gap/UK Gender Pay Gap Data - 2018 to 2019.csv')
+df_3 = pd.read_csv('Gender_Pay_Gap/UK Gender Pay Gap Data - 2019 to 2020.csv')
+df_4 = pd.read_csv('Gender_Pay_Gap/UK Gender Pay Gap Data - 2020 to 2021.csv')
+df_5 = pd.read_csv('Gender_Pay_Gap/UK Gender Pay Gap Data - 2021 to 2022-2.csv')
+df_6 = pd.read_csv('Gender_Pay_Gap/UK Gender Pay Gap Data - 2022 to 2023-3.csv')
 dfs = [df_1, df_2, df_3, df_4, df_5, df_6]
 for df in dfs:
     print(df.shape, df.columns, df.dtypes)
@@ -25,7 +19,7 @@ for df in dfs:
 # Merge datasets into a single large one and save
 df_merge = pd.concat([df_1, df_2, df_3, df_4, df_5, df_6], axis=0)
 print(df_merge.shape, df_merge.columns)
-df_merge.to_csv(r'/Users/shishengjie/Desktop/comp0035-cw-g-team-18/coursework1/gender_pay_gap_initial.csv')
+df_merge.to_csv('gender_pay_gap_initial.csv')
 
 # Delete unnecessary columns
 df_merge.drop(['Address', 'EmployerName', 'EmployerId', 'CompanyNumber', 'CompanyLinkToGPGInfo',
