0b8351c2c9c98f9674b983a33789f3c65b59c35c
jonathon0301 <115096801+jonathon0301@users.noreply.github.com>
2022-12-08 20:05:09 +0000

    adding to conftest.py

---
+++ coursework2/test/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from coursework2.src.shopping_basket import Item, Basket
+
+
+@pytest.fixture
+def item(self):
+    return Item("Brand", "Product", "Description", Decimal(10.0))
+
+
+@pytest.fixture
+def item(self):
+    return Item("Brand", "Product", "Description", Decimal(10.0))
