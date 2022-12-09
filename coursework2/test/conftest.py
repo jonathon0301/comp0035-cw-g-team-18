8bccde4c733e5c682becddbcd41e3a9691c7419e
jonathon0301 <115096801+jonathon0301@users.noreply.github.com>
2022-12-09 15:37:06 +0000

    Formatting

--- coursework2/test/conftest.py
+++ coursework2/test/conftest.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 import pytest
 from coursework2.src.shopping_basket import Item, Basket
 
+
 @pytest.fixture
 def item(self):
     return Item("Brand", "Product", "Description", Decimal(10.0))
