ddc73b4f0c0429eeb05979b9c8e7b37b89f82eba
JonathonShi <71091633+JonathonShi@users.noreply.github.com>
2022-12-09 00:51:30 +0000

    Try to let GitHub Application succeed

--- coursework2/test/conftest.py
+++ coursework2/test/conftest.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 import pytest
-from .coursework2.src.shopping_basket import Item, Basket
+from coursework2.src.shopping_basket import Item, Basket
 
 @pytest.fixture
 def item(self):
