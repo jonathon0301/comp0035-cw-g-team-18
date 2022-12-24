c6b065270221f87f7f8fd8c3007c3cf5eb1dc221
jonathon0301 <115096801+jonathon0301@users.noreply.github.com>
2022-12-24 18:07:10 +0000

    Add Given-When-Then arguments

--- coursework2/test/test_shopping_basket.py
+++ coursework2/test/test_shopping_basket.py
@@ -9,12 +9,27 @@ class TestItem:
         return Item("Brand", "Product", "Description", Decimal(10.0))
 
     def test_init(self, item):
+        """
+
+        GIVEN an item created as fixture
+        WHEN the item passes the constructor function __init__
+        THEN it will return brand_name as "Brand", product_name as "Product", description as "Description" and price as
+        10.0 in decimal
+
+        """
         assert item.brand_name == "Brand"
         assert item.product_name == "Product"
         assert item.description == "Description"
         assert item.price == Decimal(10.0)
 
     def test_repr(self, item):
+        """
+
+        GIVEN an item created as fixture
+        WHEN the item passes the string representation function __repr__
+        THEN it will return a string as "Brand, Product, Description, 10"
+
+        """
         assert repr(item) == "Brand, Product, Description, 10"
 
 
@@ -28,14 +43,44 @@ class TestBasket:
         return Item("Brand", "Product", "Description", Decimal(10.0))
 
     def test_init(self, basket):
+        """
+
+        GIVEN an empty basket created as fixture
+        WHEN it passes the constructor function __init__
+        THEN it will return an empty set of items and checkout status of False
+
+        """
         assert basket.items == {}
         assert basket.checkout is False
 
     def test_repr(self, basket, item):
+        """
+
+        GIVEN an empty basket and an item created as fixture
+        WHEN it passes the string representation function __repr__
+        THEN it will return the String representation of a basket object with iteration of the list of items
+        and their quantity in the basket, which should be False\n<Brand, Product, Description, 10, 1>'
+
+        """
         basket.add_item(item)
         assert repr(basket) == "False\n<Brand, Product, Description, 10, 1>"
 
     def test_add_item(self, basket, item):
+        """
+
+        Given a shopping basket is empty as created in fixture
+        When an item created in fixture is added to the basket with add_item method
+        Then it will return information of the item and quantity of 1
+
+        Given the basket already has 1 item created in fixture from previous shopping
+        When another of the same item is added to the basket with add_item method
+        Then it will return information of the item and quantity of 2
+
+        Given the basket contains 2 items created in fixture
+        When 3 more same items is added to the basket with add_item method
+        Then it will return information of the item and quantity of 2
+
+        """
         basket.add_item(item)
         assert basket.items == {item: 1}
         basket.add_item(item)
@@ -44,6 +89,23 @@ class TestBasket:
         assert basket.items == {item: 5}
 
     def test_remove_item(self, basket, item):
+        """
+
+        Given a shopping basket is empty as created in fixture
+        When an item created in fixture is added to the basket and then removed from the basket
+        Then it will return items as an empty set
+
+        Given a shopping basket is empty as created in fixture
+        When 3 items created in fixture are added to the basket and pass the remove_item method without
+        specifying quantity of removal
+        Then it will return items as an empty set
+
+        Given a shopping basket is empty as created in fixture
+        When 3 items created in fixture are added to the basket and pass the remove_item method while
+        specifying quantity of 2
+        Then it will return the item information and quantity as 1
+
+        """
         basket.add_item(item)
         basket.remove_item(item)
         assert basket.items == {}
@@ -55,6 +117,17 @@ class TestBasket:
         assert basket.items == {item: 1}
 
     def test_update_item(self, basket, item):
+        """
+
+        Given an item created in fixture is added into a basket with add_item method
+        When 2 more items are added to the basket with update_item method
+        Then it will return the item information and quantity as 3
+
+        Given a basket is not cleaned and has 3 items
+        When two items are now removed from the basket with update_item method
+        Then it will return the item information and quantity as 1
+
+        """
         basket.add_item(item)
         basket.update_item(item, 2)
         assert basket.items == {item: 3}
