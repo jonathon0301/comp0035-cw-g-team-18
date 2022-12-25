dd85ccf1b74988e042249d1a1ef83008807323c5
JonathonShi <71091633+JonathonShi@users.noreply.github.com>
2022-12-25 13:36:46 +0000

    Improved code based on coverage report

--- coursework2/test/conftest.py
+++ coursework2/test/conftest.py
@@ -4,10 +4,18 @@ from coursework2.src.shopping_basket import Item, Basket
 
 
 @pytest.fixture
-def item(self):
-    return Item("Brand", "Product", "Description", Decimal(10.0))
+def item_1():
+    item_1 = Item("Brand 1", "Product 1", "Description 1", Decimal(10.0))
+    yield item_1
 
 
 @pytest.fixture
-def basket(self):
-    return Basket()
+def item_2():
+    item_2 = Item("Brand 2", "Product 2", "Description 2", Decimal(20.0))
+    yield item_2
+
+
+@pytest.fixture
+def basket():
+    basket = Basket()
+    yield basket
--- coursework2/test/test_shopping_basket.py
+++ coursework2/test/test_shopping_basket.py
@@ -1,46 +1,34 @@
 from decimal import Decimal
-import pytest
-from coursework2.src.shopping_basket import Item, Basket
 
 
 class TestItem:
-    @pytest.fixture
-    def item(self):
-        return Item("Brand", "Product", "Description", Decimal(10.0))
 
-    def test_init(self, item):
+    def test_init(self, item_1):
         """
 
-        GIVEN an item created as fixture
-        WHEN the item passes the constructor function __init__
+        GIVEN an item_1 created as fixture
+        WHEN the item_1 passes the constructor function __init__
         THEN it will return brand_name as "Brand", product_name as "Product", description as "Description" and price as
         10.0 in decimal
 
         """
-        assert item.brand_name == "Brand"
-        assert item.product_name == "Product"
-        assert item.description == "Description"
-        assert item.price == Decimal(10.0)
+        assert item_1.brand_name == "Brand 1"
+        assert item_1.product_name == "Product 1"
+        assert item_1.description == "Description 1"
+        assert item_1.price == Decimal(10.0)
 
-    def test_repr(self, item):
+    def test_repr(self, item_1):
         """
 
-        GIVEN an item created as fixture
-        WHEN the item passes the string representation function __repr__
+        GIVEN an item_1 created as fixture
+        WHEN the item_1 passes the string representation function __repr__
         THEN it will return a string as "Brand, Product, Description, 10"
 
         """
-        assert repr(item) == "Brand, Product, Description, 10"
+        assert repr(item_1) == "Brand 1, Product 1, Description 1, 10"
 
 
 class TestBasket:
-    @pytest.fixture
-    def basket(self):
-        return Basket()
-
-    @pytest.fixture
-    def item(self):
-        return Item("Brand", "Product", "Description", Decimal(10.0))
 
     def test_init(self, basket):
         """
@@ -53,24 +41,24 @@ class TestBasket:
         assert basket.items == {}
         assert basket.checkout is False
 
-    def test_repr(self, basket, item):
+    def test_repr(self, basket, item_1):
         """
 
-        GIVEN an empty basket and an item created as fixture
-        WHEN it passes the string representation function __repr__
+        GIVEN an empty basket and an item_1 created as fixture is added into the basket
+        WHEN they pass the string representation function __repr__
         THEN it will return the String representation of checkout status and the list of items
         and their quantity in the basket, which should be False\n<Brand, Product, Description, 10, 1>'
 
         """
-        basket.add_item(item)
-        assert repr(basket) == "False\n<Brand, Product, Description, 10, 1>"
+        basket.add_item(item_1)
+        assert repr(basket) == "False\n<Brand 1, Product 1, Description 1, 10, 1>"
 
-    def test_add_item(self, basket, item):
+    def test_add_item(self, basket, item_1, item_2):
         """
 
         Given a shopping basket is empty as created in fixture
-        When an item created in fixture is added to the basket with add_item method
-        Then it will return information of the item and quantity of 1
+        When an item_1 created in fixture is added to the basket with add_item method
+        Then it will return information of the item_1 and quantity of 1
 
         Given the basket already has 1 item created in fixture from previous shopping
         When another of the same item is added to the basket with add_item method
@@ -81,7 +69,7 @@ class TestBasket:
         Then it will return information of the item and quantity of 5
 
         Given the basket contains 5 items created in fixture
-        When another type of item named item_1 is added to the basket with add_item method
+        When another type of item named item_2 is added to the basket with add_item method
         Then it will return information of the original item and quantity 5 with information of item_1 and quantity 1
 
         Given the basket contains 5 items created in fixture
@@ -89,22 +77,22 @@ class TestBasket:
         Then it will raise ValueError
 
         """
-        basket.add_item(item)
-        assert basket.items == {item: 1}
-        basket.add_item(item)
-        assert basket.items == {item: 2}
-        basket.add_item(item, 3)
-        assert basket.items == {item: 5}
-        item_1 = Item("Brand1", "Product1", "Description1", Decimal(10.1))
         basket.add_item(item_1)
-        assert basket.items == {item: 5,
-                                item_1: 1}
-        basket.add_item(item, -1)
-        assert "Invalid operation - Quantity must be a positive number!"
-        basket.add_item(item, 0)
-        assert "Invalid operation - Quantity must be a positive number!"
+        assert basket.items == {item_1: 1}
+        basket.add_item(item_1)
+        assert basket.items == {item_1: 2}
+        basket.add_item(item_1, 3)
+        assert basket.items == {item_1: 5}
+        basket.add_item(item_2)
+        assert basket.items == {item_1: 5,
+                                item_2: 1}
+        basket.remove_item(item_2)
+        basket.add_item(item_1, -1)
+        assert ValueError
+        basket.add_item(item_1, 0)
+        assert ValueError
 
-    def test_remove_item(self, basket, item):
+    def test_remove_item(self, basket, item_1):
         """
 
         Given a shopping basket is empty as created in fixture
@@ -126,25 +114,25 @@ class TestBasket:
         Then it will return items as an empty set
 
         """
-        basket.add_item(item)
-        basket.remove_item(item)
+        basket.add_item(item_1)
+        basket.remove_item(item_1)
         assert basket.items == {}
-        basket.add_item(item, 3)
-        basket.remove_item(item)
+        basket.add_item(item_1, 3)
+        basket.remove_item(item_1)
         assert basket.items == {}
-        basket.add_item(item, 3)
-        basket.remove_item(item, 2)
-        assert basket.items == {item: 1}
-        basket.remove_item(item, 3)
+        basket.add_item(item_1, 3)
+        basket.remove_item(item_1, 2)
+        assert basket.items == {item_1: 1}
+        basket.remove_item(item_1, 3)
         assert basket.items == {}
-        basket.add_item(item, 3)
-        basket.remove_item(item, 0)
+        basket.add_item(item_1, 3)
+        basket.remove_item(item_1, 0)
         assert basket.items == {}
-        basket.add_item(item, 3)
-        basket.remove_item(item, -1)
+        basket.add_item(item_1, 3)
+        basket.remove_item(item_1, -1)
         assert basket.items == {}
 
-    def test_update_item(self, basket, item):
+    def test_update_item(self, basket, item_1):
         """
 
         Given an item created in fixture is added into a basket with add_item method
@@ -156,13 +144,13 @@ class TestBasket:
         Then it will return an empty set
 
         """
-        basket.add_item(item)
-        basket.update_item(item, 2)
-        assert basket.items == {item: 2}
-        basket.update_item(item, -2)
+        basket.add_item(item_1)
+        basket.update_item(item_1, 2)
+        assert basket.items == {item_1: 2}
+        basket.update_item(item_1, -2)
         assert basket.items == {}
 
-    def test_view(self, basket, item, capsys):
+    def test_view(self, basket, item_1, capsys):
         """
 
         Given 2 items created in fixture are added to the shopping basket with add_item function
@@ -170,13 +158,13 @@ class TestBasket:
         Then it will return the contents of the basket including quantity, price and total cost.
 
         """
-        basket.add_item(item, 2)
+        basket.add_item(item_1, 2)
         basket.view()
         captured = capsys.readouterr()
-        assert captured.out == """---------------------\n + Brand Product - 2 x £10.00 = £20.00
+        assert captured.out == """---------------------\n + Brand 1 Product 1 - 2 x £10.00 = £20.00
 ---------------------\nBasket total = £20.00\n---------------------\n"""
 
-    def test_get_total_cost(self, basket, item):
+    def test_get_total_cost(self, basket, item_1):
         """
 
         Given two items created in fixture are added into the basket with add_item method
@@ -184,10 +172,10 @@ class TestBasket:
         Then it will return a decimal total cost for two items as 20.0
 
         """
-        basket.add_item(item, 2)
+        basket.add_item(item_1, 2)
         assert basket.get_total_cost() == Decimal(20.0)
 
-    def test_reset(self, basket, item):
+    def test_reset(self, basket, item_1):
         """
 
         Given two items created in fixture are added into the basket with add_item method
@@ -195,11 +183,11 @@ class TestBasket:
         Then it will return the basket as empty
 
         """
-        basket.add_item(item, 2)
+        basket.add_item(item_1, 2)
         basket.reset()
         assert basket.items == {}
 
-    def test_is_empty(self, basket, item):
+    def test_is_empty(self, basket, item_1):
         """
 
         Given an empty basket created in fixture
@@ -212,5 +200,5 @@ class TestBasket:
 
         """
         assert basket.is_empty() is True
-        basket.add_item(item)
+        basket.add_item(item_1)
         assert basket.is_empty() is False
